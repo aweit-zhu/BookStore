c0e36be003e6583a36c296e125a0ac72318f6512
阿偉特 <aa4192696@gmail.com>
2024-02-04 19:28:02 +0800

    update

--- src/js/data.js
+++ src/js/data.js
@@ -10,19 +10,6 @@ Book.prototype.getImageUrl = function () {
   return `https://picsum.photos/240/224?random=${this.id}`;
 };
 
-export const books = [
-  new Book(1, "IT", "Java", 400, 20),
-  new Book(2, "IT", "Javascript", 400, 10),
-  new Book(3, "IT", "Spring", 700, 25),
-  new Book(4, "IT", "Python", 350, 2),
-  new Book(5, "IT", "SQL", 400, 3),
-  new Book(6, "Lang", "華語文", 600, 20),
-  new Book(7, "Lang", "英語", 800, 6),
-  new Book(8, "Lang", "法語", 800, 4),
-  new Book(9, "Lang", "多益檢定", 1700, 20),
-  new Book(10, "Lang", "全民英檢", 1500, 50),
-];
-
 /**
  * 
  * @param {*} orderId     訂單編號
@@ -57,11 +44,37 @@ export function OrderItem(orderId,bookId,price,qty,total,status) {
   this.status = status;
 }
 
+export function Status(id,name) {
+  this.id = id;
+  this.name = name;
+}
+
+export const books = [
+  new Book(1, "IT", "Java", 400, 20),
+  new Book(2, "IT", "Javascript", 400, 10),
+  new Book(3, "IT", "Spring", 700, 25),
+  new Book(4, "IT", "Python", 350, 2),
+  new Book(5, "IT", "SQL", 400, 3),
+  new Book(6, "Lang", "華語文", 600, 20),
+  new Book(7, "Lang", "英語", 800, 6),
+  new Book(8, "Lang", "法語", 800, 4),
+  new Book(9, "Lang", "多益檢定", 1700, 20),
+  new Book(10, "Lang", "全民英檢", 1500, 50),
+];
+
+export const OrderStatus = {
+  'WAITING': new Status(1, '待出貨'),
+  'ONGOING': new Status(2, '已出貨'),
+  'ARRIVED': new Status(3, '已到門市'),
+}
+
 export const orders = [
-  new Order('20231024012313','2023/10/24 23:48:52',750.2,'已取貨'),
+  new Order('20231024012313','2023/10/24 23:48:52',750,2,OrderStatus.ONGOING),
 ];
 
 export const orderItems = [
-  new OrderItem('20231024012313',1,400,1,400,'已取貨'),
-  new OrderItem('20231024012313',4,350,1,350,'已取貨')
+  new OrderItem('20231024012313',1,400,1,400,OrderStatus.ONGOING),
+  new OrderItem('20231024012313',4,350,1,350,OrderStatus.ONGOING)
 ];
+
+
